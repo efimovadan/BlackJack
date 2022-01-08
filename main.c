025538cc7298abbd181ba843c3250166c5f777dc
pen <efimova.dan@students.dvfu.ru>
2022-01-08 22:18:28 +0300

    колода, перемешивание

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
-    printf("Hello, World!\n");
+int handValue = 0;
+
+void printArray(int a[], int size);
+int aceCheck(int aces);
+
+int main(void) {
+srand(time(NULL));
+
+
+int deck[52];
+int i;
+for (i = 0; i < 52; i++) {
+    deck[i] = i;
+}
+
+printArray(deck, 52);
+int shufDeck[52] = {0};
+int rnd, j = 0;
+
+while (j < 52) {
+    rnd = rand() % 52;
+    if (shufDeck[rnd] == 0) {
+        shufDeck[rnd] = deck[j];
+        j++;
+    }
+}
+    printArray(shufDeck, 52);
     return 0;
 }
